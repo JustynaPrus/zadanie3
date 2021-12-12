d9b05c3ea1e100ac8fb1f929c5e3c5549649f9a1
Justyna Pruś <justyna.p.prus@gmail.com>
2021-12-12 21:12:47 +0100

    Rendering one random quote

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import Button from "./Button";
-import Text from "./Text";
+import Quote from "./Text";
 
 const API = `https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json`;
 
 class App extends Component {
   state = {
     texts: [],
+    randomQuote: [],
   };
 
   componentDidMount() {
@@ -16,32 +17,22 @@ class App extends Component {
       .then((data) => this.setState({ texts: data }));
   }
 
-  handleDataFetch = () => {
-    fetch(API)
-      .then((response) => {
-        if (response.ok) {
-          return response;
-        }
-        throw Error("Niepowodzenie");
-      })
-      .then((response) => response.json())
-      .then((data) => {
-        const text = data;
-        this.setState((prevState) => ({
-          texts: prevState.texts.concat(text),
-        }));
-      });
+  randomize = () => {
+    const texts = this.state.texts;
+    const randomIndex = Math.floor(Math.random() * texts.length);
+    this.setState({ randomQuote: texts[randomIndex] });
   };
 
-  render() {
-    const texts = this.state.texts;
+  previousQuote = () => {};
 
-    const randomText = texts[Math.floor(Math.random() * texts.length)];
-    console.log(randomText);
+  render() {
+    const quotes = this.state.randomQuote;
+
+    console.log(quotes);
     return (
       <div>
-        <Button click={this.handleDataFetch} />
-        {texts ? <Text texts={texts} /> : texts}
+        <Button click={this.randomize} />
+        {quotes ? <Quote quotes={quotes} /> : quotes}
       </div>
     );
   }
